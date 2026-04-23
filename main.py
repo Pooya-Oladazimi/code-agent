4dc4a866f81b636771532ea5e992db723618bbb4
pooya <pooya.oladazimi@gmail.com>
2026-04-23 17:50:42 +0200

    finish the agent first draft

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from response import render_response
+from response import render_response, run_function_call
 from prompts import system_prompt
 from available_functions import available_functions
+import sys
 
 
 load_dotenv()
@@ -21,12 +22,24 @@ if not api_key:
 gemini_client = genai.Client(api_key=api_key)
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
-response = gemini_client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=messages,
-    config=types.GenerateContentConfig(
-        system_instruction=system_prompt, temperature=0, tools=[available_functions]
-    ),
-)
+for _ in range(20):
+    response = gemini_client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, temperature=0, tools=[available_functions]
+        ),
+    )
+    if not response.function_calls:
+        render_response(response, args.verbose)
+        sys.exit(0)
+    if response.candidates:
+        # add the previous conversation to the history
+        for can in response.candidates:
+            if can.content:
+                messages.append(can.content)
+    function_results = run_function_call(response, args.verbose)
+    messages.append(types.Content(role="user", parts=function_results))
 
-render_response(response, user_prompt, args.verbose)
+print("Agent failed to answer this query.")
+sys.exit(1)
--- response.py
+++ response.py
@@ -10,22 +10,26 @@ def render_response(response, user_prompt, verbose=False):
                 usage["prompt_token_count"], usage["candidates_token_count"]
             )
         )
-    results = []
-    if response.function_calls:
-        for func in response.function_calls:
-            f = call_function(func, verbose)
-            if len(f.parts) == 0:
-                raise Exception(f"function {f.name} parts is empty")
-            if not f.parts[0].function_response:
-                raise Exception(f"function {f.name} resp is none")
-            if not f.parts[0].function_response.response:
-                raise Exception(f"function {f.name} resp.resp is none")
-            results.append(f.parts[0])
-            if verbose:
-                print(f"-> {f.parts[0].function_response.response}")
-
     else:
         print("Response: \n{}".format(response.text))
+
+
+def run_function_call(response, verbose):
+    if not response.function_calls:
+        return []
+    results = []
+    for func in response.function_calls:
+        f = call_function(func, verbose)
+        if len(f.parts) == 0:
+            raise Exception(f"function {f.name} parts is empty")
+        if not f.parts[0].function_response:
+            raise Exception(f"function {f.name} resp is none")
+        if not f.parts[0].function_response.response:
+            raise Exception(f"function {f.name} resp.resp is none")
+        results.append(f.parts[0])
+        if verbose:
+            print(f"-> {f.parts[0].function_response.response}")
+    return results
 
 
 def extract_usage_metadata(response):
